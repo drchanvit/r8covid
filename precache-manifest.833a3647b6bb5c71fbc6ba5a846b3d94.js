82cb8d9f8299f63bf4e22a2b3214fc096dd1ad2a
drchanvit <chanvitt@gmail.com>
2020-04-11 12:50:18 +0700

    Updates

---
+++ precache-manifest.833a3647b6bb5c71fbc6ba5a846b3d94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9175fdd2c12519e6405411e9551d8423",
+    "url": "/r8covid/index.html"
+  },
+  {
+    "revision": "881a574c0d82bf835afc",
+    "url": "/r8covid/static/js/2.f3d8aafa.chunk.js"
+  },
+  {
+    "revision": "d57e74171e5ce0c9384f984a9123bbd6",
+    "url": "/r8covid/static/js/2.f3d8aafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5aad8d4af13d38d26a0",
+    "url": "/r8covid/static/js/main.3fc5e053.chunk.js"
+  },
+  {
+    "revision": "46b72107214a1c5d04d8",
+    "url": "/r8covid/static/js/runtime-main.8a68140f.js"
+  },
+  {
+    "revision": "64721f4de1b98866d0936a62e6359a99",
+    "url": "/r8covid/static/media/BrainIcon.64721f4d.svg"
+  },
+  {
+    "revision": "789307bf55781023ac1f03362788643a",
+    "url": "/r8covid/static/media/HeartIcon.789307bf.svg"
+  },
+  {
+    "revision": "1907c7aa6f64405dcb5f54db2ba2ed90",
+    "url": "/r8covid/static/media/KidneysIcon.1907c7aa.svg"
+  },
+  {
+    "revision": "25b2e5ab0e4667340e0bb722aed461a7",
+    "url": "/r8covid/static/media/LungIcon.25b2e5ab.svg"
+  }
+]);
